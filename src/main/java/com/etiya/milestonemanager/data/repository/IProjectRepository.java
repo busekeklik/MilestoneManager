8f8d276dcbf65e185ae768fc175455c2369503c9
Buse Keklik <busekeklik23@gmail.com>
2024-07-22 22:31:02 +0300

    IProjectRepository

---
+++ src/main/java/com/etiya/milestonemanager/data/repository/IProjectRepository.java
@@ -0,0 +1,9 @@
+package com.etiya.milestonemanager.data.repository;
+
+import com.etiya.milestonemanager.data.entity.ProjectEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProjectRepository extends CrudRepository<ProjectEntity, Long> {
+}
