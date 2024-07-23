13ce38c485865abcab365d91dddb062b430b7e9a
Buse Keklik <busekeklik23@gmail.com>
2024-07-23 14:17:20 +0300

    User Api

--- src/main/java/com/etiya/milestonemanager/controller/api/IUserApi.java
+++ src/main/java/com/etiya/milestonemanager/controller/api/IUserApi.java
@@ -1,4 +1,29 @@
 package com.etiya.milestonemanager.controller.api;
 
-public interface IUserApi {
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface IUserApi<D> {
+
+    // ALL DELETE
+    public ResponseEntity<String> userApiAllDelete();
+
+
+    // C R U D
+    // CREATE
+    public ResponseEntity<?>  userApiCreate(D d);
+
+    // LIST
+    public ResponseEntity<List<D>>  userApiList();
+
+    // FIND BY
+    public ResponseEntity<?>  userApiFindById(Long id);
+
+    // UPDATE
+    public ResponseEntity<?>  userApiUpdate(Long id,D d);
+
+    // DELETE
+    public ResponseEntity<?>  userApiDeleteById(Long id);
+
 }
--- src/main/java/com/etiya/milestonemanager/controller/api/impl/UserApiImpl.java
+++ src/main/java/com/etiya/milestonemanager/controller/api/impl/UserApiImpl.java
@@ -1,4 +1,63 @@
 package com.etiya.milestonemanager.controller.api.impl;
 
-public class UserApiImpl {
+import com.etiya.milestonemanager.business.dto.UserDto;
+import com.etiya.milestonemanager.business.services.IUserServices;
+import com.etiya.milestonemanager.controller.api.IUserApi;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// LOMBOK
+@RequiredArgsConstructor
+@Log4j2
+
+// API
+@RestController
+@CrossOrigin(origins = " http://localhost:3000")
+@RequestMapping("/blog/api/v1")
+
+public class UserApiImpl implements IUserApi<UserDto> {
+
+    private final IUserServices iUserServices;
+    @Override
+    @DeleteMapping(value="/delete/all")
+    public ResponseEntity<String> userApiAllDelete() {
+        iUserServices.userServiceDeleteAllData();
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    @PostMapping("/create")
+    public ResponseEntity<?> userApiCreate(UserDto userDto) {
+        return ResponseEntity.ok(iUserServices.userServiceCreate(userDto));
+    }
+
+    @Override
+    @GetMapping(value="/list")
+    public ResponseEntity<List<UserDto>> userApiList() {
+        return ResponseEntity.status(HttpStatus.OK).body(iUserServices.userServiceList());
+    }
+
+    @Override
+    @GetMapping(value="/find/{id}")
+    public ResponseEntity<?> userApiFindById(@PathVariable(name = "id") Long id) {
+        return ResponseEntity.status(200).body(iUserServices.userServiceFindById(id));
+    }
+
+    @Override
+    @PutMapping(value="/update/{id}")
+    public ResponseEntity<?> userApiUpdate(@PathVariable(name = "id") Long id, @Valid @RequestBody  UserDto userDto) {
+        return ResponseEntity.ok().body(iUserServices.userServiceUpdateById(id, userDto));
+    }
+
+    @Override
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<?> userApiDeleteById(@PathVariable(name = "id") Long id) {
+        return new ResponseEntity<>(iUserServices.userServiceDeleteById(id),HttpStatus.OK);
+    }
 }
