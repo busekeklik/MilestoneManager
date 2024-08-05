2aafc59f9f896c3ec2ef50e46e3011f177deb36a
Buse Keklik <busekeklik23@gmail.com>
2024-08-05 15:45:04 +0300

    Runner

--- src/main/java/com/etiya/milestonemanager/runner/MainRunner.java
+++ src/main/java/com/etiya/milestonemanager/runner/MainRunner.java
@@ -1,7 +1,7 @@
 package com.etiya.milestonemanager.runner;
 
 import com.etiya.milestonemanager.data.entity.ProjectEntity;
-import com.etiya.milestonemanager.data.entity.TeamEntity;
+import com.etiya.milestonemanager.data.entity.UserEntity;
 import com.etiya.milestonemanager.data.repository.IProjectRepository;
 import com.etiya.milestonemanager.data.repository.ITeamRepository;
 import com.etiya.milestonemanager.data.repository.IUserRepository;
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
 
 // LOMBOK
@@ -25,7 +24,6 @@ public class MainRunner {
 
     private final IUserRepository iUserRepository;
     private final IProjectRepository iProjectRepository;
-    private final ITeamRepository iTeamRepository;
 
     // START
     public void start(){
@@ -39,8 +37,6 @@ public class MainRunner {
             log.info("projects");
             System.out.println("users");
 
-            //Project
-
             //Calendar
             Calendar calendar = Calendar.getInstance();
             calendar.clear();
@@ -48,16 +44,47 @@ public class MainRunner {
             calendar.set(Calendar.MONTH, Calendar.JULY);
             calendar.set(Calendar.DAY_OF_MONTH, 1);
 
-
+            //PROJECT
             ProjectEntity project1 = new ProjectEntity();
             project1.setProjectName("Backend");
-            Date startDate = calendar.getTime();
-            project1.setStartDate(startDate);
-            Date endDate = calendar.getTime();
-            project1.setEndDate(endDate);
+            Date startDate1 = calendar.getTime();
+            project1.setStartDate(startDate1);
+            Date endDate1 = calendar.getTime();
+            project1.setEndDate(endDate1);
             project1.setStatus("in progress");
+            iProjectRepository.save(project1);
 
+            ProjectEntity project2 = new ProjectEntity();
+            project2.setProjectName("Frontend");
+            Date startDate2 = calendar.getTime();
+            project2.setStartDate(startDate2);
+            Date endDate2 = calendar.getTime();
+            project2.setEndDate(endDate2);
+            project2.setStatus("in progress");
+            iProjectRepository.save(project2);
+            //PROJECT
 
+            //USER
+            UserEntity user1 = new UserEntity();
+            user1.setUserName("Buse Keklik");
+            user1.setPassword("user1");
+            user1.setEmail("busekeklik@etiya.com.tr");
+            user1.setActive(true);
+            iUserRepository.save(user1);
+
+            UserEntity user2 = new UserEntity();
+            user2.setUserName("Erdem Önal");
+            user2.setPassword("user2");
+            user2.setEmail("erdemonal@etiya.com.tr");
+            user2.setActive(true);
+            iUserRepository.save(user2);
+
+            UserEntity user3 = new UserEntity();
+            user3.setUserName("Gökçen Sude Yenidünya");
+            user3.setPassword("user3");
+            user3.setEmail("gokcensudeyenidunya@etiya.com.tr");
+            user3.setActive(true);
+            iUserRepository.save(user3);
         };
     }
 }
